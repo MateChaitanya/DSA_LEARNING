050b0de956d6f4126fb6fa9ae2a760dfbd9d2399
MateChaitanya <chaitanyamate02@gmail.com>
2024-01-28 18:19:44 +0530

    Initial commit

--- src/array.java
+++ src/array.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class array {
+/*public class array {
     public static void main(String[] args) {
         //int marks[] = new int[50];
 
@@ -28,6 +28,30 @@ public class array {
         int percentage = ( marks[0]+ marks[1] + marks[2])/3;
         System.out.println("percentage = " + percentage + "%");
 
-
     }
 }
+ */
+
+//====================================================================================================================
+//Passing arrays as argument
+public class array {
+
+    //Marks will get updated by 1
+    public static void update(int marks[]){
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i]+1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[]={97,98,99};
+        update(marks);
+
+        //print our marks
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]+" ");
+        }
+        System.out.println();
+    }
+}
+
+
