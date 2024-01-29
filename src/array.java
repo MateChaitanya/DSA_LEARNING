fe74df975cde51b3c1c193f241211d385663d00e
MateChaitanya <chaitanyamate02@gmail.com>
2024-01-29 07:16:07 +0530

    Initial commit

--- src/array.java
+++ src/array.java
@@ -86,7 +86,7 @@ public class array{
 
 //================================================================================================================
 //Find the largest number in given array
-
+/*
 public class array {
 
     public static int getLargest(int numbers[]){
@@ -112,6 +112,38 @@ public class array {
 
         System.out.println("Largest Vlaue is :" + getLargest(numbers ));
 
+    }
+}
+
+ */
+//================================================================================================================
+  //Binary Search
+
+public class array{
+
+    public static int binarySearch ( int numbers[], int key){
+        int start = 0 ,end = numbers.length-1;
+
+        while(start <= end ){
+            int mid = (start + end )/2;
+
+            //compare
+            if (numbers[mid] == key ){
+                return mid;
+            }
+            if (numbers[mid]<key){
+                return mid+1;
+            }
+            else{
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[] = { 2,4,6,8,10,12,14};
+        int key = 10;
+        System.out.println("Index for key is :" + binarySearch(numbers , key));
     }
 }
 
