d7fd90b870561c0b69142bb010dadc99c09d9d06
MateChaitanya <chaitanyamate02@gmail.com>
2024-01-29 08:27:47 +0530

    Initial commit

--- src/array.java
+++ src/array.java
@@ -118,7 +118,7 @@ public class array {
  */
 //================================================================================================================
   //Binary Search
-
+/*
 public class array{
 
     public static int binarySearch ( int numbers[], int key){
@@ -146,4 +146,34 @@ public class array{
         System.out.println("Index for key is :" + binarySearch(numbers , key));
     }
 }
+ */
+//===============================================================================================================
+//Reverse an array
 
+public class array {
+
+    public static void reverse(int numbers[]){
+        int first = 0, last = numbers.length-1;
+
+        while(first < last){
+            //swap
+
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+
+        reverse(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
