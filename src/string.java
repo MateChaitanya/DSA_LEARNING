5414827dcfec756a375ae5c25274196c9f6b076d
MateChaitanya <chaitanyamate02@gmail.com>
2024-01-31 11:06:10 +0530

    first commit

--- src/string.java
+++ src/string.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.PanelUI;
 import java.util.Scanner;
 
 /*public class string {
@@ -29,14 +30,14 @@ import java.util.Scanner;
 
  */
 //==========================================================================================================
-public class string{
+/* public class string{
 
     public static boolean isPalindrome(String str){
         for (int i = 0; i < str.length()/2; i++) {
             int n = str.length();
-            if(str.charAt(i) != str.charAt(n-1-i)){
+            if(str.charAt(i) == str.charAt(n-1-i)){
                 //not a palindrome
-                return false ;
+                return false  ;
 
             }
         }
@@ -46,5 +47,43 @@ public class string{
         String str = "racecar";
         System.out.println(isPalindrome(str));
 
+    }
+}
+ */
+//============================================================================================
+//Q2.Given a short route containing 4 direction (EWNS)
+// find the shortest path to reach destination
+
+public class string{
+
+    public static float getShortestpath(String path){
+        int x = 0 , y= 0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            //South
+            if(dir =='S'){
+                y--;
+            }
+            //North
+            else if(dir =='N'){
+                y++;
+            }
+            //West
+            else if (dir =='W') {
+                x--;
+            }
+            //East
+            else {
+                x++;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float) Math.sqrt(X2 + Y2);
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestpath(path));
     }
 }
