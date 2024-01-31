4dc2a0f378f3e7963c97ad7eb071183f109c6d66
MateChaitanya <chaitanyamate02@gmail.com>
2024-01-31 10:38:26 +0530

    first commit

--- src/string.java
+++ src/string.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class string {
+/*public class string {
     public static void printLetters(String str){
         for (int i = 0; i < str.length(); i++) {
             System.out.print(str.charAt(i) + " ");
@@ -24,5 +24,27 @@ public class string {
         System.out.println(fullname.charAt(0));
 
         printLetters(fullname);
+    }
+}
+
+ */
+//==========================================================================================================
+public class string{
+
+    public static boolean isPalindrome(String str){
+        for (int i = 0; i < str.length()/2; i++) {
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                //not a palindrome
+                return false ;
+
+            }
+        }
+        return true ;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+
     }
 }
