0f478f707bf67e5e16219ed3f7f9d96ba6d031e6
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-02 21:42:02 +0530

    Initial Commit

--- src/recursion.java
+++ src/recursion.java
@@ -20,19 +20,39 @@ public class recursion {
 //=====================================================================================================================
 //Print numbers from n to 1 ( increasing order )
 
-public class recursion {
+/*public class recursion {
 
     public static void printInc( int n ){
         if( n == 1){
-            System.out.print(n+ " ");
+            System.out.println(1)  ;
             return ;
         }
         printInc(n-1);
-        System.out.print(n+ " ");
+        System.out.println(n+ " ");
     }
     public static void main(String[] args) {
         int n = 10;
         printInc(n);
 
+    }
+}
+
+ */
+//====================================================================================================
+//print factorial of a number n
+
+public class recursion {
+
+    public static int fact(int n ){
+        if(n ==0){
+            return 1;
+        }
+        int fnm1  = fact(n-1);
+        int fn = n * fact(n-1);
+        return fn;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(n));
     }
 }
