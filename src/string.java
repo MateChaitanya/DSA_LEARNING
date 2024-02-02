b53915b653d8ab2f6bf6abfb61c8d7f4623a268c
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-02 08:06:31 +0530

    first commit

--- src/string.java
+++ src/string.java
@@ -138,7 +138,7 @@ public class string{
 //"apple","mango","banana"
 //Use comapreTo function
 
-public class string {
+/*public class string {
     public static void main(String[] args) {
         String fruits[] = {"apple", "mango", "banana"};
 
@@ -149,5 +149,26 @@ public class string {
             }
         }
         System.out.println(largest);
+    }
+}
+
+ */
+//===================================================================================================================================================
+public class string{
+
+    public static boolean isPalindrome(String str){
+        for (int i = 0; i < str.length()/2; i++) {
+            int n = str.length();
+            if(str.charAt(i) == str.charAt(n-1-i)){
+                //not a palindrome
+                return false  ;
+
+            }
+        }
+        return true ;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
     }
 }
