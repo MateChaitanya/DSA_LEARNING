34e4b9ee5ab4daa7ead9f4a372f616cfaea8726d
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-03 09:54:59 +0530

    Initial Commit

--- src/recursion.java
+++ src/recursion.java
@@ -41,7 +41,7 @@ public class recursion {
 //====================================================================================================
 //print factorial of a number n
 
-public class recursion {
+/* public class recursion {
 
     public static int fact(int n ){
         if(n ==0){
@@ -54,5 +54,46 @@ public class recursion {
     public static void main(String[] args) {
         int n = 5;
         System.out.println(fact(n));
+    }
+}
+
+ */
+//=====================================================================================================================================
+//Sum of first n natural number
+/*
+public class recursion {
+
+    public static int calSum( int n ){
+        if(n == 1){
+            return 1;
+        }
+        int Snm1 = calSum(n-1);
+        int Sn = n + Snm1;
+        return Sn;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(calSum(n));
+
+    }
+}
+
+ */
+//========================================================================================================
+//Print Nth fibonacci number
+
+public class recursion{
+    public static int fib(int n ){
+        if(n == 0 || n ==1){
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 + fnm2 ;
+        return fn;
+    }
+    public static void main(String[] args) {
+        int n = 25;
+        System.out.println(fib(n));
     }
 }
