e8fff2f428958952499a4d1b710d53f5f3bdc2d2
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-04 11:31:55 +0530

    Initial Commit

--- src/recursion.java
+++ src/recursion.java
@@ -156,5 +156,33 @@ public class recursion {
 
  */
 //==================================================================================================
-//Print x^n in O(logn)
+//Tiling problem
+
+public class recursion {
+
+    public static int tillingProblem(int n ){
+        //2 x n ( floor Size)
+        //base case
+
+        if(n == 0 || n == 1){
+            return 1;
+        }
+
+        //kaam
+        //vertical choice
+
+        int fnm1 = tillingProblem(n-1);
+
+        //Horizonatl Choice
+
+        int fnm2 = tillingProblem(n-2);
+
+        int totWays = fnm1 + fnm2;
+        return totWays;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(tillingProblem(3));
+    }
+}
 
