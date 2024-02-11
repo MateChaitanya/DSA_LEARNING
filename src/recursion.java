4391edc9e9eeef03bcc4d52b8c8c58f3d6948252
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-11 10:17:53 +0530

    Initial Commit

--- src/recursion.java
+++ src/recursion.java
@@ -182,7 +182,7 @@ public class recursion {
 
     }
     public static void main(String[] args) {
-        System.out.println(tillingProblem(3));
+        System.out.println(tillingProblem(7));
     }
 }
 
