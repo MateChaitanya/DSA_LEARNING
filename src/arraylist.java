31db799c2594fc3c81c45de3ba7e9e38c52c4eba
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-16 10:10:56 +0530

    Initial Commit

---
+++ src/arraylist.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class arraylist {
+    public static void main(String[] args) {
+        int n = 10;
+
+        //Declaring the ArrayList with initial size n
+        ArrayList<Integer> arr1 = new ArrayList<>(n);
+
+        //Declaring the ArrayList
+        ArrayList<Integer> arr2 = new ArrayList<>();
+
+        //Printing the ArrayList
+
+        System.out.println("Array 1:" + arr1);
+        System.out.println("Array 2:" + arr2);
+
+
+
+    }
+}
