e29e2b711931d8b6fa154c2a5110dfe7f925452a
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-17 11:47:12 +0530

    Initial Commit

--- src/arraylist.java
+++ src/arraylist.java
@@ -234,7 +234,7 @@ class arraylist {
  */
 //===================================================================================================
 //Find Maximun in Arraylist
-
+/*
  public class arraylist{
      public static void main(String[] args) {
          ArrayList<Integer> list = new ArrayList<>();
@@ -253,3 +253,28 @@ class arraylist {
 
      }
  }
+
+ */
+//==========================================================================================
+//Swap 2 Numbers
+
+public class arraylist {
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1,list.get(idx2));
+        list.set(idx2, temp);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(6);
+        list.add(8);
+
+        int idx1 = 1, idx2 = 3;
+        System.out.println(list);
+        swap(list,idx1,idx2);
+        System.out.println(list);
+    }
+}
