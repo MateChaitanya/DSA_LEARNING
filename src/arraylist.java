368ffe51039ac45a43c34bc7dedabfd2a62767dc
MateChaitanya <chaitanyamate02@gmail.com>
2024-02-18 11:08:13 +0530

    Initial Commit

--- src/arraylist.java
+++ src/arraylist.java
@@ -257,7 +257,7 @@ class arraylist {
  */
 //==========================================================================================
 //Swap 2 Numbers
-
+/*
 public class arraylist {
     public static void swap(ArrayList<Integer> list, int idx1, int idx2){
         int temp = list.get(idx1);
@@ -275,6 +275,25 @@ public class arraylist {
         int idx1 = 1, idx2 = 3;
         System.out.println(list);
         swap(list,idx1,idx2);
+        System.out.println(list);
+    }
+}
+
+ */
+//=========================================================================================
+//Sorting an ArrayList
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(6);
+        list.add(8);
+
+        System.out.println(list);
+        Collections.sort(list);
         System.out.println(list);
     }
 }
