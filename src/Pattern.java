e6d9d6041dbe9b66aee5f1cf034a5389dffef8c4
MateChaitanya <chaitanyamate02@gmail.com>
2024-07-26 16:48:44 +0530

    Initial commit

--- src/Pattern.java
+++ src/Pattern.java
@@ -23,7 +23,7 @@ public class Pattern {
 class Pattern{
     public static void main(String[] args) {
         int n = 4;
-        for (int i = 0; i <=n; i++) {
+        for (int i = 0; i <=n-1; i++) {
             for (int j = 0; j <=i ; j++) {
                 System.out.print("*");
             }
